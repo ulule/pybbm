e8637834ea1b751d749e372fe189ca43c24532ab
Florent Messa <florent.messa@gmail.com>
2013-12-04 18:25:50 +0100

    Add filters to remove deprecation warning

--- pybb/tests/settings.py
+++ pybb/tests/settings.py
@@ -90,9 +90,15 @@ AUTH_PROFILE_MODULE = 'pybb.Profile'
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
     'handlers': {
         'mail_admins': {
             'level': 'ERROR',
+            'filters': ['require_debug_false'],
             'class': 'django.utils.log.AdminEmailHandler'
         }
     },
