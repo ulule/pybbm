fdd66bab6e1d10d1ddf5caf96956acafed417023
Florent Messa <florent.messa@gmail.com>
2017-02-06 10:31:01 +0100

    Bump

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    version='0.14.0',
+    version='0.15.0',
     description='PyBB modified. Django forum application',
     long_description=open('README.rst').read(),
     author='Florent Messa',
